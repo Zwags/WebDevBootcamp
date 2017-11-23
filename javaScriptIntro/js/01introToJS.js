53d5a86068f4716d1b6a9488996b4542db3821f9
zwags <zachary.wagner18@gmail.com>
2017-11-23 10:49:45 -0500

    Javascript tutorial started

---
+++ javaScriptIntro/js/01introToJS.js
@@ -0,0 +1,19 @@
+//var name = prompt("What is your name?");
+
+//alert("Hello " + name + ", nice to meet you!");
+//console.log("Nice to meet you " + name + ".");
+
+
+//For  excercise 1
+// var firstName = prompt("What is your first name?");
+// var lastName = prompt("What is your last name?");
+// var age = prompt("How old are you?");
+//
+//
+// alert("Hello " + firstName + " " + lastName + ", how are you?");
+// alert("I see you are " + age + " year old!");
+
+//Age calculator
+var age = prompt("How old are you?");
+var daysAlive = age*365.25;
+alert("You've been around for " + daysAlive + " days, way to go!")
