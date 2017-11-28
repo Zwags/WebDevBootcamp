293a087ad6ab807b6b36c275a59025e38224d297
zwags <zachary.wagner18@gmail.com>
2017-11-27 20:48:47 -0500

    jQuery Notes added

---
+++ 07 - JQuery/lib/01jqueryPractice.js
@@ -0,0 +1,9 @@
+
+//Make all divs purple
+$("div").css("backgroundColor", "purple")
+
+$(".highlight").css("width", "200px")
+
+$("#third").css("border", "solid 5px orange")
+
+$("div:first-of-type").css("color", "pink")
---
+++ 07 - JQuery/lib/02jQueryDemo.js
@@ -0,0 +1,30 @@
+//Did everything in console
+//
+// $("button").click(function() {
+//     alert("button clicked!");
+// });
+//
+// $("button").click(function() {
+// 	var text = $(text).text();
+//     console.log("You clicked " + text);
+// });
+//
+// $("button").click(function() {
+//     $(this).css("background", "pink")
+// });
+// $("input").keypress(function() {
+// 	console.log("you pressed key");
+// });
+//
+//
+//This is useful for getting witch, which is the code of the key pressed
+// $("input").keypress(function(event) {
+//     console.log(event);
+// });
+//13 is the enter key, throws alert
+// $("input").keypress(function(event) {
+// if(event.which === 13){
+// alert("You hit enter");
+// }
+// console.log(event);
+// });
